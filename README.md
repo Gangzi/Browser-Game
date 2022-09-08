ab0071cd4e2c9fd523e57b1b2fe04ab5b0116ed7
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-09-07 22:28:56 -0400

    Happy Run web game Ass

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Browser-Game
+
+press any key to jump, do not let the pipe hit you,have collision then game over, try how long you can keep running without collition to score.
+Refresh to restart the game.
--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let playler = document.getElementById("playler");
-let block = document.getElementById("block");
-
-function jump(){
-    playler.classList.add("jump");
-    setTimeout(function(){
-playler.classList.remove("jump");
-    },300)
+//Variables
+const player = document.getElementById("player");
+const block = document.getElementById("block");
+const score = document.getElementById("score");
+/* make the player jump*/
+function jump() {
+    player.classList.add("jump");
+    setTimeout(function () {
+        player.classList.remove("jump");
+    }, 500)
 }
-document.addEventListener("keydown", function(event){
+/* make the player jump when we press the key*/
+document.addEventListener("keypress", function () {
     jump();
 });
+/* make the palyer if has collsion */
+setInterval(() => {
+    score.innerText++;
+    /*get player position*/
+    const playerTop = parseInt(window.getComputedStyle(player)
+        .getPropertyValue('top'));
+    /* get block position*/
+    const blockLeft = parseInt(window.getComputedStyle(block)
+        .getPropertyValue('left'));
+    /*set the block disappear when runs out of the game box*/
+    if (blockLeft < 0) {
+        block.style.display = 'none';
+    } else {
+        block.style.display = '';
+    }
+    /* when collision also gets the score*/
+    if (blockLeft < 180 && blockLeft >130 && playerTop >= 190 ) {
+        alert("Your score is : " + score.innerText + 
+        "      Refresh and try again?");
+    }
+
+}, 50);
